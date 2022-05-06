f7e1631e5d38a2fc1134d71c1531da1f1e0649e8
Shahru islam <shahru@bitmascot.com>
2022-05-06 23:19:23 +0600

    Pair of each even numbers sum..

--- Numpy/numPy.py
+++ Numpy/numPy.py
@@ -1,4 +1,10 @@
+#ADD each even numbers pair and print by Numpy package
 import numpy as np
+#Get even numbers array from lists.txt by numpy
 fileData = np.loadtxt('./lists.txt', dtype=int)
+#iterate through list and add each pair
+count  = 0
 for each in fileData:
-    print(each[0], each[1])
+    count+=1
+    pairSum = each[0]+each[1]
+    print("Sum of pair no",count ,"is =", pairSum)
