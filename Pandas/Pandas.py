72effb14d3e2adb181b65d9c0dc84d0485f211fb
Shahru islam <shahru@bitmascot.com>
2022-05-07 20:46:45 +0600

    Sorted data list by pandas..

--- Pandas/Pandas.py
+++ Pandas/Pandas.py
@@ -1,4 +1,10 @@
 import pandas as pd
 #load csv data and print
 data = pd.read_csv('../Files/addresses.csv')
-print(data.to_string())
+#print files data info
+print(data.info())
+#Sort the data files by name ascending
+sorted_by_name = data.sort_values(by=["first_name"], ascending=True,inplace=False)
+#print datas
+print("--------------Original List------------\n\n",data.to_string())
+print("--------------Sorted List--------------\n\n",sorted_by_name)
